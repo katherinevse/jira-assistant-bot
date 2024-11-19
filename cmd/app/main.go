dcea8eb87a8704b0c1120ac53103585d6878b5df
katherinevse <katherinevse@mail.ru>
2024-11-19 12:52:06 +0300

    added

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,10 +1,28 @@
 package main
 
-import "github.com/katheineevse/jira-assistant-bot/internal/config"
+import (
+	"fmt"
+	"github.com/katheineevse/jira-assistant-bot/internal/config"
+	"github.com/katheineevse/jira-assistant-bot/internal/jira"
+	"log"
+)
+
+var cfg *config.Config
 
 func init() {
-	cfg := config.LoadConfig()
+	cfg = config.LoadConfig()
 }
 func main() {
+	jiraClient := jira.NewClient(cfg.JiraCfg)
+
+	//Unassign
+	unassignIssues, err := jiraClient.GetUnassignedIssues("KAN", 50)
+	if err != nil {
+		log.Fatalf("Failed to get issues: %v", err)
+	}
+
+	for _, issue := range unassignIssues {
+		fmt.Printf("Issue: %s - %s\n", issue.Key, issue.URL)
+	}
 
 }
